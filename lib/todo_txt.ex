6ae4f91b09b7f313de9cbc081379426ae206d423
Kyle San Clemente <ksanclemente@acorns.com>
2021-01-26 06:51:38 -0800

    Update parse_todo to handle done and priority tasks

--- lib/todo_txt.ex
+++ lib/todo_txt.ex
@@ -6,26 +6,68 @@ defmodule TodoTxt do
   defstruct todos: [%Todo{description: ""}], done_todos: [%Todo{description: "", done: true}]
 
   @doc """
-  parse todo.txt into a struct
+  parse_todo_txt parses a list of raw todo strings from a todo file into a
 
   ## Examples
 
-      iex> TodoTxt.parse(["x done", "todo"])
-      %TodoTxt{todos: [%Todo{description: "", done: false}], done_todos: [%Todo{description: "", done: true}]}
-
+      iex> TodoTxt.parse_todo_txt(["x done", "todo"])
+      %TodoTxt{todos: [%Todo{description: "todo", done: false}],
+        done_todos: [%Todo{description: "done", done: true}]}
   """
-  def parse(input) do
-    input.map(&parse_helper/1)
+  def parse_todo_txt(list_of_todos) do
+    {done, undone} =
+      list_of_todos
+      |> Enum.map(&parse_todo/1)
+      |> Enum.split_with(& &1.done)
+
+    %TodoTxt{todos: undone, done_todos: done}
   end
 
-  defp parse_helper(todo_string) do
-    case todo_string do
-      "x " <> description ->
-        %Todo{description: description, done: true}
+  @doc """
+  parse_todo parses a raw todo string into a Todo struct
 
-      description ->
-        %Todo{description: description, done: false}
-        model
+  ## Examples
+
+      iex> TodoTxt.parse_todo("x done")
+      %Todo{description: "done", done: true}
+
+      iex> TodoTxt.parse_todo("(A) top priority")
+      %Todo{description: "top priority", priority: :A }
+  """
+  def parse_todo(todo_string) do
+    {done_bool, undone_todo_string} = done_task_check(todo_string)
+    {priority, deprioritized_string} = priority_task_check(undone_todo_string)
+
+    %Todo{
+      description: deprioritized_string,
+      done: done_bool,
+      priority: priority
+    }
+  end
+
+  defp priority_task_check(todo_string) do
+    priority_parsed =
+      ~r/\([A-Z]\)/
+      |> Regex.split(todo_string, include_captures: true)
+      |> Enum.reject(&(&1 == ""))
+
+    if Regex.match?(~r/\([A-Z]\)/, List.first(priority_parsed)) do
+      [priority | deprioritized] = priority_parsed
+
+      {String.to_atom(List.first(Regex.run(~r/[A-Z]/, priority))),
+       String.trim(Enum.join(deprioritized, ""))}
+    else
+      {:none, todo_string}
+    end
+  end
+
+  defp done_task_check(todo_string) do
+    case todo_string do
+      "x " <> clean_todo_string ->
+        {true, clean_todo_string}
+
+      clean_todo_string ->
+        {false, clean_todo_string}
     end
   end
 end
