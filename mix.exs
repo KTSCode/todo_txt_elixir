879e98bc7f8595a8324b45f9c88a35e5219f9ba9
Kyle San Clemente <ksanclemente@acorns.com>
2021-02-13 11:32:18 -0800

    Upgrade minor Elixir version

--- mix.exs
+++ mix.exs
@@ -17,7 +17,7 @@ defmodule TodoTxt.MixProject do
       ],
       app: :todotxt_elixir,
       version: "0.1.0",
-      elixir: "~> 1.11.2",
+      elixir: "~> 1.11.3",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
