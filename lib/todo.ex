0a5815f2e1e834de53db0597a75453c7a821c5fe
Kyle San Clemente <ksanclemente@acorns.com>
2021-10-31 21:03:07 -0700

    Move parser to Todo.ex

--- lib/todo.ex
+++ lib/todo.ex
@@ -118,4 +118,158 @@ defmodule Todo do
     field_to_string = fn {key, value}, acc -> "#{acc} #{key}:#{value}" end
     Enum.reduce(additional_fields, str, field_to_string)
   end
+
+  @doc """
+  parse_todo parses a raw todo string into a Todo struct
+
+  ## Examples
+
+      iex> Todo.parse("x done")
+      %Todo{description: "done", done: true}
+
+      iex> Todo.parse("x 2020-09-13 done")
+      %Todo{description: "done", done: true, completion_date: ~D[2020-09-13]}
+
+      iex> Todo.parse("(A) top priority")
+      %Todo{description: "top priority", priority: :A }
+
+      iex> Todo.parse("todo with @Context1 and @Context_2")
+      %Todo{description: "todo with @Context1 and @Context_2", contexts: [:Context1, :Context_2]}
+
+      iex> Todo.parse("todo with +Project_1 and +project2")
+      %Todo{description: "todo with +Project_1 and +project2", projects: [:Project_1, :project2]}
+
+      iex> Todo.parse("todo 2020-10-15 with due:2021-09-13")
+      %Todo{description: "todo 2020-10-15 with", due_date: ~D[2021-09-13]}
+
+      iex> Todo.parse("2020-10-15 task due:2021-09-13")
+      %Todo{description: "task", creation_date: ~D[2020-10-15], due_date: ~D[2021-09-13]}
+
+      iex> Todo.parse("task due:2021-09-13 meta:data")
+      %Todo{description: "task", additional_fields: %{"meta" => "data"}, due_date: ~D[2021-09-13]}
+
+  """
+  def parse(str) do
+    {done_bool, completion_date, undone_str} = done_task_check(str)
+    {creation_date, creation_dateless_str} = creation_date_check(undone_str)
+    {priority, deprioritized_str} = priority_task_check(creation_dateless_str)
+    {due_date, dueless_str} = due_task_check(deprioritized_str)
+    {additional_fields, description} = additional_fields_check(dueless_str)
+
+    %Todo{
+      additional_fields: additional_fields,
+      completion_date: completion_date,
+      contexts: get_contexts(description),
+      creation_date: creation_date,
+      description: description,
+      done: done_bool,
+      due_date: due_date,
+      priority: priority,
+      projects: get_projects(description)
+    }
+  end
+
+  defp additional_fields_check(str, additional_fields \\ %{}) do
+    regex = ~r/\S*:\S*/
+    trimmed_todo = String.trim(str)
+
+    if Regex.match?(regex, trimmed_todo) do
+      [todo | add_fields_string] =
+        regex |> Regex.split(trimmed_todo, include_captures: true, trim: true)
+
+      [key | value_array] = String.split(List.last(add_fields_string), ":")
+      value = value_array |> List.to_string() |> String.trim()
+      additional_fields_check(todo, Map.put(additional_fields, key, value))
+    else
+      {additional_fields, String.trim(str)}
+    end
+  end
+
+  defp creation_date_check(str) do
+    %{date: date, todo: todo} = date_extracter(~r/^\d{4}-\d{2}-\d{2}\s/, str)
+    {date, todo}
+  end
+
+  defp due_task_check(str) do
+    %{date: date, todo: todo} = date_extracter(~r/\sdue:\d{4}-\d{2}-\d{2}/, str)
+    {date, todo}
+  end
+
+  defp get_contexts(str) do
+    ~r/\s\@\S*/
+    |> Regex.scan(str)
+    |> List.flatten()
+    |> Enum.map(&String.trim/1)
+    |> Enum.map(&String.trim(&1, "@"))
+    |> Enum.map(&String.to_atom/1)
+  end
+
+  defp get_projects(str) do
+    ~r/\s\+\S*/
+    |> Regex.scan(str)
+    |> List.flatten()
+    |> Enum.map(&String.trim/1)
+    |> Enum.map(&String.trim(&1, "+"))
+    |> Enum.map(&String.to_atom/1)
+  end
+
+  defp priority_task_check(str) do
+    priority_parsed =
+      ~r/\([A-Z]\)/
+      |> Regex.split(str, include_captures: true, trim: true)
+
+    if Regex.match?(~r/\([A-Z]\)/, List.first(priority_parsed)) do
+      [priority | deprioritized] = priority_parsed
+
+      {String.to_atom(List.first(Regex.run(~r/[A-Z]/, priority))),
+       String.trim(Enum.join(deprioritized, ""))}
+    else
+      {:none, str}
+    end
+  end
+
+  defp done_task_check(str) do
+    case str do
+      "x " <> clean_str ->
+        %{date: date, todo: todo} = date_extracter(~r/^\d{4}-\d{2}-\d{2}\s/, clean_str)
+        {true, date, todo}
+
+      clean_str ->
+        {false, :none, clean_str}
+    end
+  end
+
+  @doc """
+  helper function that takes regex and a string, then uses the regex to find a pattern in the string and extract a date from it
+
+  ## Examples
+
+      iex> Todo.date_extracter(~r/\\sdue: \\d{4}-\\d{2}-\\d{2}\/, "Give speech due: 1963-08-28")
+      %{date: ~D[1963-08-28], todo: "Give speech"}
+
+      iex> Todo.date_extracter(~r/^\\d{4}-\\d{2}-\\d{2}\\s/, "1963-08-28 Give speech")
+      %{date: ~D[1963-08-28], todo: "Give speech"}
+
+      iex> Todo.date_extracter(~r/^\\d{4}-\\d{2}-\\d{2}\\s/, "1963-08-28 Give speech, save the date: 1964-10-14")
+      %{date: ~D[1963-08-28], todo: "Give speech, save the date: 1964-10-14"}
+
+  """
+
+  def date_extracter(regex, todo_string_with_date) do
+    split = Regex.split(regex, todo_string_with_date, include_captures: true, trim: true)
+
+    date =
+      split
+      |> Enum.reject(&(!Regex.match?(regex, &1)))
+      |> List.to_string()
+      |> (fn string -> Regex.scan(~r/\d{4}-\d{2}-\d{2}/, string) end).()
+      |> List.to_string()
+
+    todo = split |> Enum.reject(&Regex.match?(regex, &1)) |> List.to_string()
+
+    case Date.from_iso8601(date) do
+      {:ok, valid_date} -> %{date: valid_date, todo: todo}
+      _ -> %{date: :none, todo: todo_string_with_date}
+    end
+  end
 end
--- test/todo_test.exs
+++ test/todo_test.exs
@@ -21,4 +21,24 @@ defmodule TodoTest do
 
     assert Todo.to_string(todo) == expected_string
   end
+
+  test "parse" do
+    expected_todo = %Todo{
+      description: "This test has everything",
+      additional_fields: %{"key1" => "value1", "key2" => "value2"},
+      done: true,
+      completion_date: ~D[2020-09-13],
+      priority: :A,
+      contexts: [:context1, :context2],
+      projects: [:project1, :project2],
+      creation_date: ~D[2021-09-13],
+      due_date: ~D[2021-09-13],
+      pomodori: {1, 3}
+    }
+
+    string =
+      "x (A) 2020-09-13 2021-09-13 This test has everything @context1 @context2 +project1 +project2 due:2021-09-13 key1:value1 key2:value2 (#pomo: 1/3)"
+
+    assert Todo.parse(string) == expected_todo
+  end
 end
