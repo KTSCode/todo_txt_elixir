c6cce67b92931b08b69d3a01b9fe526c6532861c
Kyle San Clemente <kylesanclemente@gmail.com>
2023-01-11 20:31:39 -0800

    Update Git Hooks Config and Clean Up Config Files

--- config/config.exs
+++ config/config.exs
@@ -1,1 +1,28 @@
+import Config
 
+# somewhere in your config file
+if Mix.env() == :dev do
+  config :git_hooks,
+    auto_install: true,
+    verbose: true,
+    branches: [
+      blacklist: ["main"]
+    ],
+    hooks: [
+      pre_commit: [
+        tasks: [
+          {:cmd, "mix format --check-formatted"}
+        ]
+      ],
+      pre_push: [
+        verbose: false,
+        tasks: [
+          {:cmd, "mix clean"},
+          {:cmd, "mix compile --warnings-as-errors"},
+          {:cmd, "mix credo --strict"},
+          {:cmd, "mix test --color"},
+          {:cmd, "echo 'success!'"}
+        ]
+      ]
+    ]
+end
